22eb3fad0681b5026c96ebac8f684e8c9afb8d20
DOni5503 <dboyer5503@gmail.com>
2024-10-29 20:04:59 -0400

    user routes completed

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,33 +1,78 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
 
-export const getAllUsers = async (req: Request, res: Response) => {
+export const getAllUsers = async (_req: Request, res: Response) => {
     try {
         const users = await User.find({});
         res.json(users);
     } catch (err) {
         res.status(400).json(err);
     }
-    };
+};
 
-export const getUserbyId = async (req: Request, res: Response) => {
+export const getSingleUser = async (req: Request, res: Response) => {
     try {
         const user = await User.findOne({ _id: req.params.id });
         res.json(user);
     } catch (err) {
         res.status(400).json(err);
     }
-    };
+};
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const user = await User.create(req.body);
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email
+        });
         res.json(user);
     } catch (err) {
         res.status(400).json(err);
     }
-    };
+};
 
 export const updateUser = async (req: Request, res: Response) => {
     try {
-        const user = await User.findOneAndUpdate({ _id: req.params.id });
+        const user = await User.findOneAndUpdate({ _id: req.params.id },
+            req.body, { new: true })
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.params.id });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const addFriend = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        );
+
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const deleteFriend = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
